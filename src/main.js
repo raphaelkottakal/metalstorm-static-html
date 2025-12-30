dc8812684bbfded049ee60866fc7b8d11f36a2c7
Raphael Kottakal <raphaelkottakal@Raphaels-MacBook-Pro.local>
2025-12-31 00:16:26 +0530

    add animation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as THREE from "../node_modules/three/build/three.module.js";
+import gsap from "../node_modules/gsap/all.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -9,15 +10,23 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+const canvasContainer = document.querySelector("#app");
+const state = {
+  canClickCube: true,
+};
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 const devicePixelRatio = Math.min(window.devicePixelRatio, 2);
 renderer.setPixelRatio(devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
+canvasContainer.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+const geometry = new THREE.IcosahedronGeometry(1, 2);
+const material = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  flatShading: true,
+});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
@@ -28,7 +37,7 @@ scene.add(ambientLight);
 
 const keyLight = new THREE.DirectionalLight(0xffffff);
 keyLight.position.set(-2, 1, 2);
-keyLight.intensity = 4;
+keyLight.intensity = 2;
 scene.add(keyLight);
 // const keyLightHelper = new THREE.DirectionalLightHelper(keyLight, 1, 0x00ff00);
 // scene.add(keyLightHelper);
@@ -50,3 +59,36 @@ function animate() {
 
   renderer.render(scene, camera);
 }
+
+const cubeAnimationTimeline = gsap.timeline({
+  paused: true,
+  onComplete: () => {
+    state.canClickCube = true;
+  },
+});
+
+cubeAnimationTimeline.to(cube.scale, {
+  duration: 1,
+  ease: "elastic.out(1,0.3)",
+  x: 2,
+  y: 2,
+  z: 2,
+});
+
+cubeAnimationTimeline.to(cube.scale, {
+  duration: 1,
+  ease: "slow(0.7,0.7,false)",
+  x: 1,
+  y: 1,
+  z: 1,
+});
+
+function handleClick() {
+  if (state.canClickCube) {
+    cubeAnimationTimeline.restart();
+    state.canClickCube = false;
+  }
+}
+
+canvasContainer.addEventListener("click", handleClick);
+canvasContainer.addEventListener("touch", handleClick);
